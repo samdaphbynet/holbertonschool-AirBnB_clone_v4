3372d9709385b418ac8766e17c7f48550d9fce42
samdaphbynet <lj8@hotmail.fr>
2023-09-25 19:02:56 +0000

    Select some Amenities to be comfortable!

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/')
+@app.route('/1-hbnb/')
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -35,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            cache_id=uuid.uuid4(),
                            states=st_ct,
                            amenities=amenities,
